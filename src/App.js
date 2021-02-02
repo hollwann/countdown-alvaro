bdc774cc4bc482962833d361c15b54ae3253b948
Hollwann <hollwann@hotmail.com>
2021-02-02 16:47:42 -0500

    extend 1 hour

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Countdown from 'react-countdown';
 
 function App() {
-  const finalDate = new Date('2/2/2021 5:00 PM')
+  const finalDate = new Date('2/2/2021 6:00 PM')
   return (
     <div>
       <p>Cuenta regresiva decisión de Alvaro Mendez: </p>
