239e01188f9880530bfc2c93a1361f284d5575e8
Hollwann <hollwann@hotmail.com>
2021-06-11 19:06:21 -0500

    add video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import Countdown from 'react-countdown';
+import "./App.css";
+import Countdown from "react-countdown";
+import videoAlvaro from "./assets/video.mp4";
 
 function App() {
-  const finalDate = new Date('06/14/2021 1:00 PM')
+  const finalDate = new Date("06/14/2021 1:00 PM");
   return (
-    <div>
+    <div style={{ display: "flex", flexDirection: "column" }}>
       <p>Cuenta regresiva decisión de Alvaro Mendez: </p>
       <Countdown date={finalDate} />
+      <video
+        width="500"
+        height="500"
+        autoPlay
+        muted
+        style={{ marginTop: 30 }}
+        loop
+      >
+        <source src={videoAlvaro} type="video/mp4" />
+      </video>
     </div>
   );
 }
